248b67aa89ba83eb2910c1b9b157e96a5ad70ae8
Aftab Alam git config --global user.email alamaftab9426@gmail.com <your-email@gmail.com>
2026-08-11 23:35:06 +0530

    Change App.js error

--- server/app.js
+++ server/app.js
@@ -2,8 +2,17 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express();
+// Super Admin Routes
+import authRoutes from "./routes/authRoutes.js";
+import companyRoutes from "./routes/superAdminRoutes/companyRoutes.js";
+import subscriptionRoutes from "./routes/superAdminRoutes/subcriptionRoutes.js";
 
+// Admin Routes
+import departmentRoutes from "./routes/admin/departmentRoutes.js";
+import createEmployeeRoutes from "./routes/admin/createEmployeeRoutes.js";
+
+
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -17,26 +26,50 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      // Postman / server-to-server requests
       if (!origin) {
         return callback(null, true);
       }
-
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
-
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "PATCH",
+      "DELETE",
+      "OPTIONS",
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+    ],
   })
 );
-
 app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "AI ERP Backend Running Successfully",
+  });
+});
+
+app.get("/api/health", (req, res) => {
   res.status(200).json({
     success: true,
     message: "Server Running Successfully",
   });
 });
+// SUPER ADMIN ROUTES
+app.use("/api/auth", authRoutes);
+app.use("/api/company", companyRoutes);
+app.use("/api/subscription", subscriptionRoutes);
+
+// ADMIN ROUTES
+app.use("/api/admin/department", departmentRoutes);
+app.use("/api/admin/employee", createEmployeeRoutes);
+
+
+export default app;
